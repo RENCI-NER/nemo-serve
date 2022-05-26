c77d5cb40bbb13d9c314073a197838850debb9ac
Yaphetkg <yaphetkg@renci.org>
2022-05-26 09:37:36 -0400

    first iteration of the model api server

---
+++ src/ModelSingleton.py
@@ -0,0 +1,70 @@
+
+import logging
+
+# from nemo.collections.nlp.models import TokenClassificationModel
+
+logger = logging.Logger("model-loader")
+
+
+class ModelNotFoundError(Exception):
+    pass
+
+
+class ModelWrapper:
+    """ Inherit this class and do any model intialization here"""
+    def __init__(self):
+
+        pass
+
+    def __call__(self, query_text):
+        """ Make a call to initialized model's Predict function"""
+        raise NotImplementedError("Call to wrapped model is required")
+
+
+class TokenClassificationModelWrapper(ModelWrapper):
+    def __init__(self, model_path):
+        """ Initializes NLP Model"""
+        super(TokenClassificationModelWrapper, self).__init__()
+        # self.model = TokenClassificationModel.restore_from(model_path)
+
+    def __call__(self, query_text):
+        """ Runs prediction on text"""
+        return self.model.add_predictions([query_text])
+
+
+class ModelFactory:
+    models = {}
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def load_model(name, path, model_class):
+        if name in ModelFactory.models.keys():
+            logger.info(f"Model {name} already in class skipping initialization")
+            return
+        else:
+            # @TODO replace call with actual model init call
+            logger.info(f"Initializing model {name}")
+            assert issubclass(model_class, ModelWrapper), "Error please provide a subclass type of ModelWrapper"
+            # initializes model and makes its prediction a callable
+            ModelFactory.models[name] = model_class(path)
+
+    @staticmethod
+    def query_model(model_name, query_text):
+        if model_name not in ModelFactory.models:
+            raise ModelNotFoundError(f"Model {model_name} not found")
+        # @TODO return model stuff
+        # since we have model as a callable class we can just treat it like a function
+        return ModelFactory.models[model_name](query_text)
+
+    @staticmethod
+    def get_model_names():
+        return list(ModelFactory.models.keys())
+
+
+if __name__ == '__main__':
+    model_path = ""
+    ModelFactory.load_model('medmentions', model_path=model_path, model_class=TokenClassificationModelWrapper)
+    result = ModelFactory.query_model('medmentions', "asthma is a disease")
+    print(result)
---
+++ src/server.py
@@ -0,0 +1,37 @@
+import os
+
+from fastapi import FastAPI
+from src.ModelSingleton import ModelFactory, TokenClassificationModelWrapper
+from src.ApiDataStructs import Query
+import uvicorn
+import logging
+
+app = FastAPI()
+logger = logging.Logger("server")
+
+
+@app.on_event("startup")
+def init_nlp_model():
+    logger.info("Loading nlp model")
+    path = os.environ.get("MODEL_PATH")
+    ModelFactory.load_model(name="token_classification", path=path, model_class=TokenClassificationModelWrapper)
+    logger.info(f"Loaded token_classification model from {path}")
+
+
+@app.post("/annotate/")
+async def annotate_text(query: Query):
+    if query.model_name not in ModelFactory.get_model_names():
+        return {"error": f"please provide a valid model name from {ModelFactory.get_model_names()}"}, 400
+    return ModelFactory.query_model(model_name=query.model_name, query_text=query.text)
+
+
+@app.get("/models/")
+async def get_model_names():
+    """
+    """
+    return ModelFactory.get_model_names()
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, port=8080)
