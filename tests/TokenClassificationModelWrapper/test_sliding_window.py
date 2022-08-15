1f60090347c4b1e8bffb63059a76a2543db45ca1
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-08-15 18:53:33 -0400

    Passing tests but confusing result.

--- tests/TokenClassificationModelWrapper/test_sliding_window.py
+++ tests/TokenClassificationModelWrapper/test_sliding_window.py
@@ -9,26 +9,78 @@ from unittest.mock import Mock
 def test_basic():
     tcmw = Mock(spec=TokenClassificationModelWrapper)
 
-    lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " \
-                  "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " \
-                  "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit " \
-                  "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " \
-                  "sunt in culpa qui officia deserunt mollit anim id est laborum. "
+    # In this example text, every sentence has one more word than the previous sentence.
+    example = "Word. A sentence. Three word sentence. Four words in sentence. Five words in this sentence."
 
-    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, lorem_ipsum, 2)) == [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
-        'dolore magna aliqua.',
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est '
-        'laborum.'
+    # Setting the window_size to 1 returns all sentences separately.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example, 1)) == [
+        "Word.", "A sentence.", "Three word sentence.", "Four words in sentence.", "Five words in this sentence."
     ]
 
-    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, lorem_ipsum, 50)) == [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
-        'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex '
-        'ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat '
-        'nulla pariatur.',  # 52 words
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est '
-        'laborum.'
+    # Setting the window_size to 2 causes the first two sentences to be merged.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example, 2)) == [
+        "Word. A sentence.",            # 3 words
+        "Three word sentence.",         # 3 words
+        "Four words in sentence.",      # 4 words
+        "Five words in this sentence."  # 5 words
+    ]
+
+    # Setting the window_size to 3 still causes only the first two sentences to be merged.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example, 3)) == [
+        "Word. A sentence.",                # 3 words
+        "Three word sentence.",             # 3 words
+        "Four words in sentence.",          # 4 words
+        "Five words in this sentence."      # 4 words
+    ]
+
+    # We have to increase the window_size to 4 before the next sentence will be merged in.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example, 4)) == [
+        "Word. A sentence. Three word sentence.",   # 5 words
+        "Four words in sentence.",                  # 4 words
+        "Five words in this sentence."              # 5 words
+    ]
+
+    # In this example, the sentences have decreasing size.
+    example_decreasing = "Five words in this sentence. Four words in sentence. Three word sentence. A sentence. Word."
+
+    # Setting the window_size to 1 returns all sentences separately.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 1)) == [
+        "Five words in this sentence.", "Four words in sentence.", "Three word sentence.", "A sentence.", "Word."
+    ]
+
+    # Setting the window size to 2 has no effect.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 2)) == [
+        "Five words in this sentence.", "Four words in sentence.", "Three word sentence.", "A sentence.", "Word."
+    ]
+
+    # But setting the window size to 3 causes the last two sentences to be merged together.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 3)) == [
+        "Five words in this sentence.",     # 5 words
+        "Four words in sentence.",          # 4 words
+        "Three word sentence.",             # 3 words
+        "A sentence. Word."                 # 3 words
+    ]
+
+    # ... and so on.
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 4)) == [
+        "Five words in this sentence.",         # 5 words
+        "Four words in sentence.",              # 4 words
+        "Three word sentence. A sentence.",     # 5 words
+        "Word."                                 # 1 word
+    ]
+
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 5)) == [
+        "Five words in this sentence.",                     # 5 words
+        "Four words in sentence. Three word sentence.",     # 7 words
+        "A sentence. Word."                                 # 3 words
+    ]
+
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 6)) == [
+        "Five words in this sentence. Four words in sentence.",     # 9 words
+        "Three word sentence. A sentence. Word."                    # 6 words
+    ]
+
+    assert list(TokenClassificationModelWrapper.sliding_window(tcmw, example_decreasing, 7)) == [
+        "Five words in this sentence. Four words in sentence.",     # 9 words
+        "Three word sentence. A sentence. Word."                    # 6 words
     ]
