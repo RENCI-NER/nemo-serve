332cac84ef25ae5fc5727da9e65f6037e2b338d5
hyi <hongyi@renci.org>
2022-11-23 11:07:20 -0500

    add count input parameter to annotate endpoint

--- src/ApiDataStructs.py
+++ src/ApiDataStructs.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
 
-
 class Query(BaseModel):
     text: str
     model_name: str
+    # count is not required and will default to 1 if nothing is passed
+    count: int = 1
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "text": "input text for prediction",
+                "model_name": "tokenclassification or sapbert",
+                "count": 1
+            }
+        }
--- src/ModelSingleton.py
+++ src/ModelSingleton.py
@@ -182,7 +182,7 @@ class SapbertModelWrapper(ModelWrapper):
         self.all_reps_names = all_reps_name_ids['Name']
         self.all_reps_ids = all_reps_name_ids['ID']
 
-    def __call__(self, query_text):
+    def __call__(self, query_text, count):
         """ Runs prediction on text"""
         toks = self.tokenizer.batch_encode_plus([query_text], padding="max_length", max_length=25, truncation=True,
                                                 return_tensors="pt")
@@ -192,8 +192,12 @@ class SapbertModelWrapper(ModelWrapper):
         output = self.model(**toks_cuda)
         cls_rep = output[0][:, 0, :]
         dist = cdist(cls_rep.cpu().detach().numpy(), self.all_reps_emb)
-        nn_index = np.argmin(dist)
-        return [self.all_reps_names[nn_index], self.all_reps_ids[nn_index]]
+        if count == 1:
+            nn_index = np.argmin(dist)
+            return [self.all_reps_names[nn_index], self.all_reps_ids[nn_index]]
+
+        sorted_dist = np.argsort(dist)[:count]
+        return [[self.all_reps_names[idx], self.all_reps_ids[idx]] for idx in sorted_dist]
 
 
 class ModelFactory:
@@ -226,11 +230,11 @@ class ModelFactory:
                 ModelFactory.models[name] = model_class(path)
 
     @staticmethod
-    def query_model(model_name, query_text):
+    def query_model(model_name, query_text, query_count=1):
         if model_name not in ModelFactory.models:
             raise ModelNotFoundError(f"Model {model_name} not found")
         # since we have model as a callable class we can just treat it like a function
-        return ModelFactory.models[model_name](query_text)
+        return ModelFactory.models[model_name](query_text, query_count)
 
     @staticmethod
     def get_model_names():
